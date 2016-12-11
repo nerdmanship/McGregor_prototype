258f00dc427f21b199ec251e438b118d42c80275
Johan <johan.eu.stromqvist@gmail.comgit config --global user.emailgit config --global user.email johan.eu.stromqvist@gmail.com>
2016-12-11 19:46:13 +0800

    Updated wrist movement

--- _assets/js/mcgregorModule.js
+++ _assets/js/mcgregorModule.js
@@ -1,16 +1,16 @@
 const mcgregorModule = function() {
   // Cache DOM
-  const mcgregor = document.querySelector("[data-mcgregorArt]"); console.log(mcgregor);
-  const head = mcgregor.querySelector("[data-head]"); console.log(head);
-  const neck = mcgregor.querySelector("[data-neck]"); console.log(neck);
-  const body = mcgregor.querySelector("[data-body]"); console.log(body);
-  const waist = mcgregor.querySelector("[data-waist]"); console.log(waist);
-  const leftShoulder = mcgregor.querySelector("[data-leftShoulder]"); console.log(leftShoulder);
-  const leftElbow = mcgregor.querySelector("[data-leftElbow]"); console.log(leftElbow);
-  const leftWrist = mcgregor.querySelector("[data-leftWrist]"); console.log(leftWrist);
-  const rightShoulder = mcgregor.querySelector("[data-rightShoulder]"); console.log(rightShoulder);
-  const rightElbow = mcgregor.querySelector("[data-rightElbow]"); console.log(rightElbow);
-  const rightWrist = mcgregor.querySelector("[data-rightWrist]"); console.log(rightWrist);
+  const mcgregor = document.querySelector("[data-mcgregorArt]");
+  const head = mcgregor.querySelector("[data-head]");
+  const neck = mcgregor.querySelector("[data-neck]");
+  const body = mcgregor.querySelector("[data-body]");
+  const waist = mcgregor.querySelector("[data-waist]");
+  const leftShoulder = mcgregor.querySelector("[data-leftShoulder]");
+  const leftElbow = mcgregor.querySelector("[data-leftElbow]");
+  const leftWrist = mcgregor.querySelector("[data-leftWrist]");
+  const rightShoulder = mcgregor.querySelector("[data-rightShoulder]");
+  const rightElbow = mcgregor.querySelector("[data-rightElbow]");
+  const rightWrist = mcgregor.querySelector("[data-rightWrist]");
 
   let joints = [head, neck, body, waist, leftWrist, leftElbow, leftShoulder, rightWrist, rightElbow, rightShoulder]
   
@@ -28,11 +28,11 @@ const mcgregorModule = function() {
   
   TweenMax.fromTo(leftShoulder, dur, { x: 10, y: -10, rotation: -50 }, { x: -10, y: 10, rotation: 60, repeat: -1, yoyo: true });
   TweenMax.fromTo(leftElbow, dur, { rotation: -20 }, { rotation: 70, repeat: -1, yoyo: true });
-  TweenMax.fromTo(leftWrist, dur, { rotation: -5 }, { rotation: 10, repeat: -1, yoyo: true });
+  TweenMax.fromTo(leftWrist, dur, { rotation: 0 }, { rotation: 10, repeat: -1, yoyo: true });
 
   TweenMax.fromTo(rightShoulder, dur, { x: -10, y: 10, rotation: 60 }, { x: 10, y: -10, rotation: -50, repeat: -1, yoyo: true });
   TweenMax.fromTo(rightElbow, dur, { rotation: 70 }, { rotation: -20, repeat: -1, yoyo: true });
-  TweenMax.fromTo(rightWrist, dur, { rotation: 10 }, { rotation: -5, repeat: -1, yoyo: true });
+  TweenMax.fromTo(rightWrist, dur, { rotation: 10 }, { rotation: 0, repeat: -1, yoyo: true });
 }
 
 mcgregorModule();
